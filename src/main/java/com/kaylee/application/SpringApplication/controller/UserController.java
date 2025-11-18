585eb6e88a2ed8f56809ac8cff59bce4ba026f03
Kaylee <ks0754@mcla.edu>
2025-11-18 14:46:21 -0500

    user controller addition

---
+++ src/main/java/com/kaylee/application/SpringApplication/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.kaylee.application.SpringApplication.controller;
+
+import com.kaylee.application.SpringApplication.model.User;
+import com.kaylee.application.SpringApplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setName(userDetails.getName());
+            user.setAge(userDetails.getAge());
+            user.setWeight(userDetails.getWeight());
+            user.setHeight(userDetails.getHeight());
+            return userRepository.save(user);
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+}
