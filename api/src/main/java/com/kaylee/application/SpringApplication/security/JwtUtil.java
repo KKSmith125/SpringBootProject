f684b18d8313635ee03de0e108472502f64d8bde
Kaylee <ks0754@mcla.edu>
2025-11-18 15:54:53 -0500

    backend login logic and some front

---
+++ api/src/main/java/com/kaylee/application/SpringApplication/security/JwtUtil.java
@@ -0,0 +1,48 @@
+package com.kaylee.application.SpringApplication.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long EXPIRATION = 1000 * 60 * 60 * 24;
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            parse(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    private Jws<Claims> parse(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+    }
+}
--- ui/src/actions/userAction.js
+++ ui/src/actions/userAction.js
@@ -2,4 +2,20 @@ import {authenticate, unauthenticate} from '../components/slices/userSlice';
 import store from '../store.js';
 import axios from 'axios';
 
-const verifyToken = () => {}
+axios.defaults.baseURL = 'http://localhost:8080';
+
+const login = async (email, password) => {
+  try {
+    const response = await axios.post('/users/login', {email, password});
+
+    store.dispatch(authenticate(response.data));
+
+    console.log('Successfully logged in:', response.data);
+  }
+  catch (error) {
+    console.error('Failed login:', error.response?.data);
+    store.dispatch(unauthenticate);
+  }
+};
+
+export {login};
